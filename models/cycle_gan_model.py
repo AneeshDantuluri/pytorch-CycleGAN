f62723df381c5d5d0ea6dce8dabafd42797e323f
Aneesh Dantuluri <aneesh72583@gmail.com>
2023-12-18 19:29:08 -0800

    print A and B paths

--- models/cycle_gan_model.py
+++ models/cycle_gan_model.py
@@ -112,6 +112,9 @@ class CycleGANModel(BaseModel):
         self.real_A = input['A' if AtoB else 'B'].to(self.device)
         self.real_B = input['B' if AtoB else 'A'].to(self.device)
         self.image_paths = input['A_paths' if AtoB else 'B_paths']
+         
+        print("A_paths", input["A_paths"])
+        print("B_paths", input["B_paths"])
 
     def forward(self):
         """Run forward pass; called by both functions <optimize_parameters> and <test>."""
