e86f7044e5cd0efe26e32798031d18408a1df6b1
Aneesh Dantuluri <aneesh72583@gmail.com>
2023-12-18 18:48:53 -0800

    supervise->supervised

--- options/train_options.py
+++ options/train_options.py
@@ -36,6 +36,6 @@ class TrainOptions(BaseOptions):
         parser.add_argument('--lr_policy', type=str, default='linear', help='learning rate policy. [linear | step | plateau | cosine]')
         parser.add_argument('--lr_decay_iters', type=int, default=50, help='multiply by a gamma every lr_decay_iters iterations')
         # add supervise option when training model
-        parser.add_argument('--supervise', action='store_true', help='whether training on paired or unpaired image data')
+        parser.add_argument('--supervised', action='store_true', help='whether training on paired or unpaired image data')
         self.isTrain = True
         return parser
